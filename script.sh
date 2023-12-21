b5227e049d65dc954e27977d09fbce9caabc2652
SathishKumarNagaraj <sathishkumarnagaraj12@gmail.com>
2023-12-21 15:26:01 +0000

    script sh change

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Export the GitHub username
-export GITHUB_USERNAME="<username>"
+export GITHUB_USERNAME="SathishKumarNagaraj"
 
 # Export the GitHub repository name
-export GITHUB_REPOSITORY="se2001-<roll_number>"
+export GITHUB_REPOSITORY="se2001-22f2001467"
